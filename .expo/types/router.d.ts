5f60dc3e8d79e9e80f5ebc97acddaadc3b4d8c78
joseph <white.joseph.jr@gmail.com>
2023-12-01 21:42:04 -0600

    completed basic welcome screen

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -7,7 +7,7 @@ declare module "expo-router" {
   export * from 'expo-router/build';
 
   // prettier-ignore
-  type StaticRoutes = `/` | `/home`;
+  type StaticRoutes = `/` | `/_layout` | `/home`;
   // prettier-ignore
   type DynamicRoutes<T extends string> = never;
   // prettier-ignore
