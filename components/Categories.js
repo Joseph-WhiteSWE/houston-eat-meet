1cabcf8214f45f75ba5f11c03cfb0e4896975bd2
joseph <white.joseph.jr@gmail.com>
2023-12-14 02:53:12 -0600

    added explore img

--- components/Categories.js
+++ components/Categories.js
@@ -6,88 +6,90 @@ export default function Categories() {
     {
       id: "0",
       name: "Brunch",
-      // image: require("../assets/images/hem.jpg"),
+      image: require("../assets/images/hem-images/pancake.png"),
     },
     {
       id: "1",
       name: "Restuarants",
-      // image: require("../assets/images/hem.jpg"),
+      image: require("../assets/images/hem-images/taco.png"),
     },
     {
       id: "3",
       name: "Seafood",
-      // image: "https://www.flaticon.com/free-icons/cereal",
+      image: require("../assets/images/hem-images/salmon.png"),
     },
     {
       id: "4",
       name: "International",
-      // image: "https://www.flaticon.com/free-icons/cereal",
+      image: require("../assets/images/hem-images/bento.png"),
     },
     {
       id: "5",
       name: "Soul Food",
-      // image: "https://www.flaticon.com/free-icons/cereal",
+      image: require("../assets/images/hem-images/roast-chicken.png"),
     },
     {
       id: "6",
       name: "BBQ",
-      // image: "https://www.flaticon.com/free-icons/cereal",
+      image: require("../assets/images/hem-images/skewer.png"),
     },
     {
       id: "7",
       name: "Sweet Tooth",
-      // image: "https://www.flaticon.com/free-icons/cereal",
+      image: require("../assets/images/hem-images/cake-yum.png"),
     },
     {
       id: "8",
-      name: "Vibes",
-      // image: "https://www.flaticon.com/free-icons/cereal",
+      name: "Views",
+      image: require("../assets/images/hem-images/fuji-mountain.png"),
     },
     {
       id: "9",
       name: "Bars",
-      // image: "https://www.flaticon.com/free-icons/cereal",
+      image: require("../assets/images/hem-images/cocktail.png"),
     },
     {
       id: "10",
       name: "Cigars",
-      // image: "https://www.flaticon.com/free-icons/cereal",
+      image: require("../assets/images/hem-images/cigar.png"),
     },
   ];
   return (
     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
       {groups.map((item, index) => (
-        <View
-          key={index}
-          style={{
-            width: 90,
-            height: 40,
-            // borderColor: "#E0E0E0",
-            borderWidth: 1,
-            paddingVertical: 5,
-            paddingHorizontal: 1,
-            borderRadius: 5,
-            marginHorizontal: 10,
-            marginVertical: 10,
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: "#c026d3",
-          }}
-        >
-          {/* <Image
-            source={{ uri: item.image }}
+        <View>
+          <View
+            key={index}
             style={{
-              width: 50,
-              height: 50,
+              width: 60,
+              height: 60,
+              borderColor: "#E0E0E0",
+              borderWidth: 1,
+              paddingVertical: 5,
+              paddingHorizontal: 1,
+              borderRadius: 5,
+              marginHorizontal: 10,
+              marginVertical: 10,
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "black",
             }}
-          /> */}
-
+          >
+            <Image
+              source={item.image}
+              style={{
+                width: 50,
+                height: 50,
+              }}
+            />
+          </View>
           <Text
             style={{
               color: "white",
               fontSize: 13,
               fontWeight: "500",
               marginVertical: 6,
+              textAlign: "center",
             }}
           >
             {item?.name}
