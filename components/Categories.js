eb4e5545f3eb4b76c69bd8c2705c505c7cd5e7d6
joseph <white.joseph.jr@gmail.com>
2023-12-14 03:20:49 -0600

    x

--- components/Categories.js
+++ components/Categories.js
@@ -4,52 +4,52 @@ import React from "react";
 export default function Categories() {
   const groups = [
     {
-      id: "0",
+      id: 0,
       name: "Brunch",
       image: require("../assets/images/hem-images/pancake.png"),
     },
     {
-      id: "1",
+      id: 1,
       name: "Restuarants",
       image: require("../assets/images/hem-images/taco.png"),
     },
     {
-      id: "3",
+      id: 3,
       name: "Seafood",
       image: require("../assets/images/hem-images/salmon.png"),
     },
     {
-      id: "4",
+      id: 4,
       name: "International",
       image: require("../assets/images/hem-images/bento.png"),
     },
     {
-      id: "5",
+      id: 5,
       name: "Soul Food",
       image: require("../assets/images/hem-images/roast-chicken.png"),
     },
     {
-      id: "6",
+      id: 6,
       name: "BBQ",
       image: require("../assets/images/hem-images/skewer.png"),
     },
     {
-      id: "7",
+      id: 7,
       name: "Sweet Tooth",
       image: require("../assets/images/hem-images/cake-yum.png"),
     },
     {
-      id: "8",
+      id: 8,
       name: "Views",
       image: require("../assets/images/hem-images/fuji-mountain.png"),
     },
     {
-      id: "9",
+      id: 9,
       name: "Bars",
       image: require("../assets/images/hem-images/cocktail.png"),
     },
     {
-      id: "10",
+      id: 10,
       name: "Cigars",
       image: require("../assets/images/hem-images/cigar.png"),
     },
@@ -59,7 +59,7 @@ export default function Categories() {
       {groups.map((item, index) => (
         <View>
           <View
-            key={item.id}
+            key={index}
             style={{
               width: 60,
               height: 60,
